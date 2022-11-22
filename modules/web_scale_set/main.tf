4224e5730f941f1ce6013e9ec0d3a058630767f8
dawright22 <53329137+dawright22@users.noreply.github.com>
2022-11-22 12:14:42 +1100

    Update main.tf

--- modules/web_scale_set/main.tf
+++ modules/web_scale_set/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "web_scale_set" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    sku       = "20.04-LTS"
     version   = "latest"
   }
 
